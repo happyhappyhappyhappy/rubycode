7adb849807205c6bf1177febb309fdfb5569f7aa
ymnkkj <ymnkkj@gmail.com>
2019-10-22 18:38:46 +0900

    ABC081B 1回目完了 201910221838

--- atcoderruby/begineersSelection/chap3/ABC081B_1.rb
+++ atcoderruby/begineersSelection/chap3/ABC081B_1.rb
@@ -2,7 +2,27 @@
 # Reference https://www.javadrive.jp/ruby/array/index3.html
 # Reference https://atcoder.jp/contests/abc081/submissions/8063993
 # Ruby 1st Try
-N = gets.to_i # ひとまず要らない気がするが
+N = gets.to_i 
 A = gets.chomp.split(' ').map(&:to_i)
-p A
-p A.size
+# p A
+# p A.size
+oddFlag = false
+answer = 0
+while true
+   for j in 0..(N-1) do
+     data = A[j]
+     mod = data % 2
+    if (mod != 0) 
+        oddFlag = true
+    end
+   end
+   if(oddFlag) 
+        break
+   else
+    answer=answer+1
+   end
+   for j in 0..(N-1) do
+       A[j] = A[j] / 2
+   end
+end
+puts(answer)
