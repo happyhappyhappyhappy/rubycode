3e667c50df3d93c5ecf5db4888bada79a11fbf2e
ymnkkj <ymnkkj@gmail.com>
2019-10-26 19:44:54 +0900

    ABC068B 2回目途中 201910261944

---
+++ atcoderruby/begineersSelection/chap3/ABC068B_2.rb
@@ -0,0 +1,21 @@
+# Problem https://atcoder.jp/contests/abc068/tasks/abc068_b
+# Ruby 2nd Try
+Nmax = 100
+Ni = Array.new(Nmax+1,0)
+1.upto(Nmax) do |j|
+    number = j
+    counter = 0
+    oddFlag = false
+    while true
+        if number % 2 == 1
+        oddFlag = true
+        end
+        if oddFlag
+            break
+        end
+        number = number / 2
+        counter = counter +1
+    end
+    Ni[j]=counter
+end
+# p Ni
