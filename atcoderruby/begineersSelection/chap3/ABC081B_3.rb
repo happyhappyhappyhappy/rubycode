b884fefb61b5d3e78aa1ae4969ca3371600aabbc
ymnkkj <ymnkkj@gmail.com>
2019-10-26 16:43:25 +0900

    ABC081B 3回目終了 201910261643

--- atcoderruby/begineersSelection/chap3/ABC081B_3.rb
+++ atcoderruby/begineersSelection/chap3/ABC081B_3.rb
@@ -4,6 +4,26 @@ N = gets.chomp.to_i
 ValueDiv = Array.new(N,0)
 Ai = gets.chomp.split(' ').map(&:to_i)
 
-for k in 0..(Ai.length) do
+for k in 0..(Ai.length-1) do
     oddFlag = false
+    number = Ai[k]
+    while true
+        modNumber = number % 2
+        if (modNumber == 1)
+            oddFlag = true
+        end
+        if oddFlag
+            break
+        else
+            number = number / 2
+            ValueDiv[k] = ValueDiv[k]+1
+        end
+    end
 end
+answer = 1000
+for k in 0..(Ai.length-1) do
+    if ValueDiv[k] < answer
+        answer = ValueDiv[k]
+    end
+end
+puts(answer)
