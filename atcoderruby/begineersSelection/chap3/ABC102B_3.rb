3e0627b5a291faf27bb4d634232cc7f88622ec47
ymnkkj <ymnkkj@gmail.com>
2019-11-02 15:47:28 +0900

    ABC102B 3回目終了 201911021547

--- atcoderruby/begineersSelection/chap3/ABC102B_3.rb
+++ atcoderruby/begineersSelection/chap3/ABC102B_3.rb
@@ -5,3 +5,13 @@ ai = gets.chomp.split(' ').map(&:to_i)
 maxnum = 1
 minnum =  10**9
 # p minnum
+for j in 0..(n-1) do
+    if maxnum <= ai[j]
+        maxnum = ai[j]
+    end
+    if ai[j] <= minnum
+        minnum = ai[j]
+    end
+end
+answer = (maxnum-minnum).abs
+puts(answer)
