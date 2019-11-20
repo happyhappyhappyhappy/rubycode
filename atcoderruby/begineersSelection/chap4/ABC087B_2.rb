6eee69522e139ab0b9bfcce1e05e97c9129eadf6
ymnkkj <ymnkkj@gmail.com>
2019-11-20 19:54:12 +0900

    ABC087B 2回目途中 201911201954

---
+++ atcoderruby/begineersSelection/chap4/ABC087B_2.rb
@@ -0,0 +1,18 @@
+# Problem https://atcoder.jp/contests/abc087/tasks/abc087_b
+# Ruby 2nd Try
+have500y = 0
+have100y = 0
+have050y = 0
+total = 0
+answer = 0
+have500y = gets.chomp().to_i
+have100y = gets.chomp().to_i
+have050y = gets.chomp().to_i
+total = gets.chomp().to_i
+for j in 0...have500y do
+    for k in 0...have100y do
+        for m in 0...have050y do
+            
+        end
+    end
+end
