460308f8e768e2a19cc1342b90e91dff7e0a6dc3
ymnkkj <ymnkkj@gmail.com>
2019-11-26 18:29:24 +0900

    ABC095B 3回目終了 rubyの練習もひとまずペンディング 201911261829 reset --soft HEAD^

--- atcoderruby/begineersSelection/chap3/ABC095B_3.rb
+++ atcoderruby/begineersSelection/chap3/ABC095B_3.rb
@@ -5,3 +5,12 @@ mi = []
 for j in 0...N do
     mi << gets.chomp().to_i
 end
+mmin = mi.min
+misum = 0
+for j in 0...N do
+    misum = misum+mi[j]
+end
+answer = N
+rquan = X - misum
+answer = answer +  rquan / mmin
+print(answer)
